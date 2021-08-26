bc5340f34cd499dbd631173f7cdfde95512a1c06
iksflow <theiksflow@gmail.com>
2021-08-26 23:52:10 +0900

    learn init variables

--- ch02/var_example2.sh
+++ ch02/var_example2.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-OS_TYPE="redhat"
+
+echo ${OS_TYPE:-redhat}
+echo ${OS_TYPE}
+
+echo ${OS_TYPE2:=centos}
+echo ${OS_TYPE2}
---
+++ ch02/var_example3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo ${1?argument required}
+echo ${1:?not null argument required}
